be788768ba1691e27771532c870e8be83af7afff
Kittitouchhh <s66111596@gmail.com>
2025-10-31 20:42:58 +0700

    update

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -22,7 +22,7 @@ const User: React.FC<datauser> = ({name,image}) => {
             <img className=" cursor-pointer hover:scale-120 hover:m-3 
             ml-6 w-[35px] h-[35px] rounded-full self-center"
             onClick={handleCrownClick}
-            src="/public/member/crownlogo.png" alt="" />
+            src="/public/member/crownlogo.png" alt="membership" />
 
             <img src={image || 'https://cdn-icons-png.flaticon.com/256/6522/6522516.png'} alt="" className='w-[50px] h-[50px] rounded-full self-center'/>
             <p className='  self-center text-white text-[18px]'>{name} </p>
--- src/page/memberpage.tsx
+++ src/page/memberpage.tsx
@@ -1,6 +1,25 @@
 export default function Memberpage(){
     return(
         <>
+            <div className="flex w-full h-[300px]  justify-center">
+                <div className="bg-[#28221F] w-1/2 h-[300px]">
+                    <p>WELCOME TO MEMBERSHIP</p>
+                    <p> Welcome to Membership! We're delighted to
+                        have you join our community. As a member,
+                         you'll enjoy exclusive discounts, early
+                         access to promotions, personalized 
+                         recommendations, and surprise gifts 
+                         made just for you. Our goal is to make 
+                         your journey with us more rewarding, 
+                         enjoyable, and meaningful. Thank you 
+                         for choosing to be with us — we look 
+                         forward to sharing this exciting 
+                         journey together.
+                    </p>
+                </div>
+                    <img src="/public/user/memberpic.png" alt="member" />
+                
+            </div>
         </>
     )
 }
